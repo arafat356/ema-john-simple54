90fbe67b0d4262d9edb5154971ef684f86c5cfb1
arafat356 <ahmedarafatft@gmail.com>
2022-08-05 17:43:59 +0600

    add some comments

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,29 @@ import OrderReview from './components/OrderReview/OrderReview';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import Register from './components/Register/Register';
 import Shop from './components/Shop/Shop';
+import AuthProvider from './context/AuthProvider';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header> </Header>
-        <Routes>
-          <Route exact path="/" element={<Shop></Shop>}></Route>
-          <Route path="/shop" element={<Shop></Shop>}></Route>
-          <Route path="/review" element={<OrderReview></OrderReview>}></Route>
-          <Route path="/inventory" element={<Inventory> </Inventory>}></Route>
-          <Route
-            path="/placeorder"
-            element={<PlaceOrder> </PlaceOrder>}
-          ></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/register" element={<Register></Register>}></Route>
-          <Route path="*" element={<NotFound> </NotFound>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header> </Header>
+          <Routes>
+            <Route exact path="/" element={<Shop></Shop>}></Route>
+            <Route path="/shop" element={<Shop></Shop>}></Route>
+            <Route path="/review" element={<OrderReview></OrderReview>}></Route>
+            <Route path="/inventory" element={<Inventory> </Inventory>}></Route>
+            <Route
+              path="/placeorder"
+              element={<PlaceOrder> </PlaceOrder>}
+            ></Route>
+            <Route path="/login" element={<Login></Login>}></Route>
+            <Route path="/register" element={<Register></Register>}></Route>
+            <Route path="*" element={<NotFound> </NotFound>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -16,22 +16,27 @@ Step-1 Initial Setup
 4.initializefirebase
 5.Enable auth method
 ------------------
-Step-2 
+Step-2 set up componenets
 1.create login componenet
 2.create register componenet
 3.create route for login and register
  
 ---------------------------
-step-3
+step-3 set auth system
 1.set up sign in method 
-2. setup sign out method 
-3. user state 
+2.setup sign out method 
+3.user state 
 4.special observer
 4.return nessary methods and states from useFirebase
 --------------------------------
-step:4
+step:4 crate auth context hook
 1.create a auth context 
 2.create context provider
 3.use auth provider
-.4.create useAuth hook
+4.create useAuth hook
+5.set context provider context value
+-------------------------------
+step:5 create private route
+1.create private route
+2.set private route
 */
